aebf6c455af444f157d051c45300c0e3ddf862a5
eyael <eyaelja1@gmail.com>
2019-02-28 16:59:22 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Todolist.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todolist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=5)
+    private String name;
+
+    @NotNull
+    @Size(min=5)
+    private String description;
+
+    @NotNull
+    @Size(min=5)
+    private String priority;
+
+    @NotNull
+    @Size(min=5)
+    private String duedate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(String duedate) {
+        this.duedate = duedate;
+    }
+}
---
+++ src/main/java/com/example/demo/TodolistRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TodolistRepository extends CrudRepository<Todolist,
+        Long>{
+
+}
+
